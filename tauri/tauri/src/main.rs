f9000614fb4daff71d71c0ffab8d2f81fd9c9403
Shixin Huang <shixinhuang99@gmail.com>
2025-04-01 19:34:05 +0800

    update tauri

--- tauri/tauri/src/main.rs
+++ tauri/tauri/src/main.rs
@@ -3,6 +3,7 @@
 fn main() {
 	tauri::Builder::default()
 		.invoke_handler(tauri::generate_handler![])
+		.plugin(tauri_plugin_opener::init())
 		.run(tauri::generate_context!())
 		.expect("Failed to launch app");
 }
